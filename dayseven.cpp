6ca9a8be359cbc8bf0ed5cb2820673563cab28ce
lampros <lamprosfasoulas@gmail.com>
2021-04-19 02:31:15 +0300

    dayseven is a thing

---
+++ dayseven.cpp
@@ -0,0 +1,18 @@
+/**
+ * Date: 18/04/2021
+ * Day: 7 / 100
+ * 
+ * Creator: Lampros Fasoulas
+ * 
+ * -- Project Description --
+ * this is my first cpp program 
+ * its just a hello world i wanna go to sleep
+ */
+
+#include <iostream>
+
+
+int main(){
+    std::cout << "Hello, World!";
+    return 0;
+}
